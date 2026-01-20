2d66a2aedd262ef17054036ddebb555d40b4d209
Julian Kuhlmann <kuhlmann@mpp.mpg.de>
2026-01-20 16:49:08 +0100

    add events of other icecube seasons

--- src/Hnu.jl
+++ src/Hnu.jl
@@ -1,10 +1,9 @@
 module Hnu
 
 export PointSource
-
-include("events/events.jl")
-include("sources/sources.jl")
 include("utils/roi.jl")
 include("utils/utils.jl")
+include("events/events.jl")
+include("sources/sources.jl")
 
 end
--- src/events/events.jl
+++ src/events/events.jl
@@ -4,10 +4,27 @@ using DelimitedFiles
 using SkyCoords
 using Unitful
 
-using ..utils.ROI
+import ..ROI: CircularROI
 
 const BASEPATH = "/Users/David/.icecube_data/20210126_PS-IC40-IC86_VII/icecube_10year_ps/events"
-const IC40 = string(BASEPATH, "/", "IC40_exp.csv")
+
+const IC40_PATH = joinpath(BASEPATH, "IC40_exp.csv")
+const IC59_PATH = joinpath(BASEPATH, "IC59_exp.csv")
+const IC79_PATH = joinpath(BASEPATH, "IC79_exp.csv")
+const IC86_I_PATH = joinpath(BASEPATH, "IC86_I_exp.csv")
+const IC86_II_PATH = joinpath(BASEPATH, "IC86_II_exp.csv")
+const IC86_III_PATH = joinpath(BASEPATH, "IC86_III_exp.csv")
+const IC86_IV_PATH = joinpath(BASEPATH, "IC86_IV_exp.csv")
+const IC86_V_PATH = joinpath(BASEPATH, "IC86_V_exp.csv")
+const IC86_VI_PATH = joinpath(BASEPATH, "IC86_VI_exp.csv")
+const IC86_VII_PATH = joinpath(BASEPATH, "IC86_VII_exp.csv")
+
+const IC40 = 1
+const IC59 = 2
+const IC79 = 3
+const IC86_I = 4
+const IC86_II = 5
+
 
 mutable struct EventList
     mjd::Vector
@@ -16,14 +33,29 @@ mutable struct EventList
     ang_err::Vector
     coords::Vector
     energy::Vector
+    detector::Vector
     N::Int
 end
 
-EventList(mjd, ra, dec, ang_err, energy) = EventList(mjd, ra, dec, ang_err, ICRSCoords.(ra, dec), energy, length(energy))
+EventList(mjd, ra, dec, ang_err, energy, detector) = EventList(mjd, ra, dec, ang_err, ICRSCoords.(ra, dec), energy, detector, length(energy))
 
 function loadEvents(fname::String)
+    if "40" occursin(fname)
+        detector = IC40
+    elseif "59" occursin(fname)
+        detector = IC59
+    elseif "79" occursin(fname)
+        detector = IC79
+    elseif "IC86_II" occursin(fname)
+        detector = IC86_II
+    elseif "IC86_I" occursin(fname)
+        detector = IC86_I
+    else
+        println("unsupported detector")
+    end
     events = readdlm(fname, Float64, comments=true, comment_char='#')
-    EventList(events[:, 1], events[:, 4]u"deg", events[:, 5]u"deg", events[:, 3]u"deg", events[:, 2])
+    detector_vector = Vector{Int}(detector, lenght(events[:, 2]))
+    EventList(events[:, 1], events[:, 4]u"deg", events[:, 5]u"deg", events[:, 3]u"deg", events[:, 2], detector_vector)
 end
 
 function selectEvents!(events::EventList, roi::CircularROI)
